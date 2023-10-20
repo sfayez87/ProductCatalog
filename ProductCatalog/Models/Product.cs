8aa2834c3cb960f4c0fbe33491ddd23d26a234cf
SFaiz <SFaiz@NSF-333>
2023-10-20 18:13:06 +0300

    Add project files.

---
+++ ProductCatalog/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime Duration { get; set; }
+        public decimal Price { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
